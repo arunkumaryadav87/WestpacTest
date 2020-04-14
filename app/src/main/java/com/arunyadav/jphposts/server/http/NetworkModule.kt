cd97926c4004f5910b5443196e0bd03add23513c
Arun.Yadav <Arun.Yadav@youtap.com>
2020-04-14 12:28:50 +1200

    Added caching to API calls

--- app/src/main/java/com/arunyadav/jphposts/server/http/NetworkModule.kt
+++ app/src/main/java/com/arunyadav/jphposts/server/http/NetworkModule.kt
@@ -5,7 +5,8 @@ import com.arunyadav.jphposts.R
 import com.arunyadav.jphposts.server.rx.SchedulerProvider
 import com.arunyadav.jphposts.server.rx.SchedulerProviderImpl
 import com.readystatesoftware.chuck.ChuckInterceptor
-import okhttp3.OkHttpClient
+import okhttp3.*
+import okhttp3.CacheControl
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,15 +14,13 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 
 class NetworkModule {
-
     fun provideRetrofit(): Retrofit {
-        val client: OkHttpClient = getOkHttpClient()
+        val client = getOkHttpClient()
         return Retrofit.Builder()
-            .addConverterFactory(ScalarsConverterFactory.create()) //string parser to always be on top
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(provideHTTPSBaseURL())
             .client(client)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
     }
 
@@ -30,8 +29,12 @@ class NetworkModule {
     }
 
     private fun getOkHttpClient(): OkHttpClient {
+        val cacheSize = (1 * 1024 * 1024).toLong()
+        val appCache = Cache(JPHPostsApplication.getAppContext().cacheDir, cacheSize)
         return try {
             val okHttpClientBuilder = OkHttpClient.Builder()
+                .addNetworkInterceptor(cacheInterceptor)
+                .cache(appCache)
                 .addInterceptor(ChuckInterceptor(JPHPostsApplication.getAppContext()))
                 .readTimeout(
                     TIMEOUT_VALUE.toLong(),
@@ -44,11 +47,26 @@ class NetworkModule {
         }
     }
 
+    private val cacheInterceptor = Interceptor { chain: Interceptor.Chain ->
+        val response = chain.proceed(chain.request())
+        val cacheControl = CacheControl.Builder()
+            .maxAge(5, TimeUnit.MINUTES) // online caching
+            .maxStale(5, TimeUnit.MINUTES)  // Offline caching
+            .build()
+        response.newBuilder()
+            .removeHeader(HEADER_PRAGMA)
+            .removeHeader(HEADER_CACHE_CONTROL)
+            .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+            .build()
+    }
+
     private fun provideHTTPSBaseURL(): String =
         JPHPostsApplication.getAppContext().getString(R.string.base_url)
 
     companion object {
         private const val TAG = "NetworkModule"
+        private const val HEADER_PRAGMA = "pragma"
+        private const val HEADER_CACHE_CONTROL = "cache-control"
         private const val TIMEOUT_VALUE = 60
     }
 }
